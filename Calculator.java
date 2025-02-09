fc5a7ed23061336f28edd84718528d5e69d1239d
aditibaghel9 <asbaghel@quinnipiac.edu>
2025-02-09 14:29:27 -0500

    Asked user for two numbers and then gave user an option for addition or subtraction

--- Calculator.java
+++ Calculator.java
@@ -4,22 +4,64 @@ public class Calculator{
 
 Scanner sc = new Scanner(System.in);
 
-public void Addition(){
-
-    System.out.println("Enter two whole numbers");
+public int getNum1(){
 
     int n1 = sc.nextInt();
+
+    return n1;
+}
+
+public int getNum2(){
+
     int n2 = sc.nextInt();
 
-    int sum = n1+n2;
+    return n2;
+}
 
-    System.out.println(n1 + "+" + n2 + "=" + sum);
+public String getOption(){
+
+    String o1 = sc.next();
+
+    return o1;
+}
+
+
+public void Addition(int b1, int b2){
+
+    int sum = b1+b2;
+
+    System.out.println(b1 + "+" + b2 + "=" + sum);
+} 
+
+public void Subtraction(int s1, int s2){
+
+    int dif = s1-s2;
+
+    System.out.println(s1 + "-" + s2 + "=" + dif);
 } 
 
     public static void main(String[] args) {
         
        Calculator obj = new Calculator();
 
-       obj.Addition();
+       System.out.println("Enter two whole numbers");
+
+       int a1 = obj.getNum1();
+
+       int a2 = obj.getNum2();
+
+       System.out.println("Would you like the numbers to perform addition or subtraction, put + for addition or - for subtraction?");
+       
+       String o1 = obj.getOption();
+
+       if(o1.equals("+")){
+
+        obj.Addition(a1, a2);
+       }
+
+       if(o1.equals("-")){
+
+        obj.Subtraction(a1, a2);
+       }
     }
 }
